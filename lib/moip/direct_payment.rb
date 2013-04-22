c3bf2baac8282e90ee26f446c6ca6f92a9cc5cdc
Pitágoras G. Galiotto <pitagg@gmail.com>
2013-04-22 13:44:51 -0300

    Forma não é obrigatório pelo MoIP.OF

--- lib/moip/direct_payment.rb
+++ lib/moip/direct_payment.rb
@@ -32,104 +32,60 @@ module MoIP
   TiposComInstituicao = %w{CartaoCredito CartaoCredito DebitoBancario}
 
   class DirectPayment
-
     class << self
-
       # Cria uma instrução de pagamento direto
       def body(attributes = {})
-
-#raise "#{attributes[:valor]}--#{attributes[:valor].to_f}"
+        #raise "#{attributes[:valor]}--#{attributes[:valor].to_f}"
         raise(MissingPaymentTypeError, "É necessário informar a razão do pagamento") if attributes[:razao].nil?
         raise(MissingPayerError, "É obrigatório passar as informarções do pagador") if attributes[:pagador].nil?
-
         raise(InvalidValue, "Valor deve ser maior que zero.") if attributes[:valor].to_f <= 0.0
         raise(InvalidPhone, "Telefone deve ter o formato (99)9999-9999.") if attributes[:pagador][:tel_fixo] !~ /\(\d{2}\)?\d{4}-\d{4}/
         raise(InvalidCellphone, "Telefone celular deve ter o formato (99)9999-9999.") if attributes[:pagador][:tel_cel] !~ /\(\d{2}\)?\d{4}-\d{4}/
-
         raise(MissingBirthdate, "É obrigatório passar as informarções do pagador") if TiposComInstituicao.include?(attributes[:forma]) && attributes[:data_nascimento].nil?
-
         raise(InvalidExpiry, "Data de expiração deve ter o formato 01-00 até 12-99.") if TiposComInstituicao.include?(attributes[:forma]) && attributes[:expiracao] !~ /(1[0-2]|0\d)\/\d{2}/
-
-
         raise(InvalidReceiving, "Recebimento é inválido. Escolha um destes: #{TipoRecebimento.join(', ')}") if !TipoRecebimento.include?(attributes[:recebimento]) && TiposComInstituicao.include?(attributes[:forma])
-
         raise(InvalidInstitution, "A instituição #{attributes[:instituicao]} é inválida. Escolha uma destas: #{InstituicaoPagamento.join(', ')}") if  TiposComInstituicao.include?(attributes[:forma]) && !InstituicaoPagamento.include?(attributes[:instituicao])
 
         builder = Nokogiri::XML::Builder.new(:encoding => "UTF-8") do |xml|
-
           # Identificador do tipo de instrução
-          xml.EnviarInstrucao {
-            xml.InstrucaoUnica(TipoValidacao: "Transparente") {
-
+          xml.EnviarInstrucao do
+            xml.InstrucaoUnica(TipoValidacao: "Transparente") do
               # Dados da transação
-              xml.Razao {
-                xml.text attributes[:razao]
-              }
-              xml.Valores {
-                xml.Valor(:moeda => "BRL") {
-                  xml.text attributes[:valor]
-                }
-              }
-              xml.IdProprio {
-                xml.text attributes[:id_proprio]
-              }
-
+              xml.Razao { xml.text attributes[:razao] }
+              xml.Valores do
+                xml.Valor(:moeda => "BRL") { xml.text attributes[:valor] }
+              end
+              xml.IdProprio { xml.text attributes[:id_proprio] }
               # Definindo o pagamento direto
-              xml.PagamentoDireto {
-                xml.Forma {
-                  xml.text attributes[:forma]
-                }
-
-                # Débito Bancário
-                if ["DebitoBancario"].include?(attributes[:forma])
-                  xml.Instituicao {
-                    xml.text attributes[:instituicao]
-                  }
+              xml.PagamentoDireto do
+                if attributes[:forma].present?
+                  xml.Forma { xml.text attributes[:forma] }
+                  # Débito Bancário
+                  xml.Instituicao { xml.text attributes[:instituicao] } if ["DebitoBancario"].include?(attributes[:forma])
+                  # Cartão de Crédito
+                  if attributes[:forma] == "CartaoCredito"
+                    xml.Instituicao { xml.text attributes[:instituicao] }
+                    xml.CartaoCredito do
+                      xml.Numero { xml.text attributes[:numero] }
+                      xml.Expiracao { xml.text attributes[:expiracao] }
+                      xml.CodigoSeguranca { xml.text attributes[:codigo_seguranca] }
+                      xml.Portador do
+                        xml.Nome { xml.text attributes[:nome] }
+                        xml.Identidade(:Tipo => "CPF") { xml.text attributes[:identidade] }
+                        xml.Telefone { xml.text attributes[:telefone] }
+                        xml.DataNascimento { xml.text attributes[:data_nascimento] }
+                      end
+                    end
+                    xml.Parcelamento do
+                      xml.Parcelas { xml.text attributes[:parcelas] }
+                      xml.Recebimento { xml.text attributes[:recebimento] }
+                    end
+                  end
                 end
-
-                # Cartão de Crédito
-                if attributes[:forma] == "CartaoCredito"
-                  xml.Instituicao {
-                    xml.text attributes[:instituicao]
-                  }
-                  xml.CartaoCredito {
-                    xml.Numero {
-                      xml.text attributes[:numero]
-                    }
-                    xml.Expiracao {
-                      xml.text attributes[:expiracao]
-                    }
-                    xml.CodigoSeguranca {
-                      xml.text attributes[:codigo_seguranca]
-                    }
-                    xml.Portador {
-                      xml.Nome {
-                        xml.text attributes[:nome]
-                      }
-                      xml.Identidade(:Tipo => "CPF") {
-                        xml.text attributes[:identidade]
-                      }
-                      xml.Telefone {
-                        xml.text attributes[:telefone]
-                      }
-                      xml.DataNascimento {
-                        xml.text attributes[:data_nascimento]
-                      }
-                    }
-                  }
-                  xml.Parcelamento {
-                    xml.Parcelas {
-                      xml.text attributes[:parcelas]
-                    }
-                    xml.Recebimento {
-                      xml.text attributes[:recebimento]
-                    }
-                  }
-                end
-              }
+              end
 
               # Dados do pagador
-              xml.Pagador {
+              xml.Pagador do
                 xml.Nome { xml.text attributes[:pagador][:nome] }
                 xml.LoginMoIP { xml.text attributes[:pagador][:login_moip] }
                 xml.Email { xml.text attributes[:pagador][:email] }
@@ -137,7 +93,7 @@ module MoIP
                 xml.TelefoneCelular { xml.text attributes[:pagador][:tel_cel] }
                 xml.Apelido { xml.text attributes[:pagador][:apelido] }
                 xml.Identidade(:Tipo => "CPF") { xml.text attributes[:pagador][:identidade] }
-                xml.EnderecoCobranca {
+                xml.EnderecoCobranca do
                   xml.Logradouro { xml.text attributes[:pagador][:logradouro] }
                   xml.Numero { xml.text attributes[:pagador][:numero] }
                   xml.Complemento { xml.text attributes[:pagador][:complemento] }
@@ -147,48 +103,27 @@ module MoIP
                   xml.Pais { xml.text attributes[:pagador][:pais] }
                   xml.CEP { xml.text attributes[:pagador][:cep] }
                   xml.TelefoneFixo { xml.text attributes[:pagador][:tel_fixo] }
-                }
-              }
+                end
+              end
 
               # Boleto Bancario
               if attributes[:forma] == "BoletoBancario"
                 # Dados extras
-                xml.Boleto {
-                  xml.DiasExpiracao(:Tipo => "Corridos") {
-                    xml.text attributes[:dias_expiracao]
-                  }
-                  xml.Instrucao1 {
-                    xml.text attributes[:instrucao_1]
-                  }
-                  xml.URLLogo {
-                    xml.text attributes[:url_logo]
-                  }
-                }
+                xml.Boleto do
+                  xml.DiasExpiracao(:Tipo => "Corridos") { xml.text attributes[:dias_expiracao] }
+                  xml.Instrucao1 { xml.text attributes[:instrucao_1] }
+                  xml.URLLogo { xml.text attributes[:url_logo] }
+                end
               end
-              
-              if attributes[:url_retorno]
-                # URL de retorno
-                xml.URLRetorno {
-                  xml.text attributes[:url_retorno]
-                }
-              end
-              
-              if attributes[:url_notificacao]
-                # URL de retorno
-                xml.URLNotificacao {
-                  xml.text attributes[:url_notificacao]
-                }
-              end
-                
-            }
-          }
+              # URL de retorno
+              xml.URLRetorno { xml.text attributes[:url_retorno] } if attributes[:url_retorno]
+              # URL de retorno
+              xml.URLNotificacao { xml.text attributes[:url_notificacao] } if attributes[:url_notificacao]
+            end
+          end
         end
-
         builder.to_xml
       end
-
     end
-
   end
-
 end
